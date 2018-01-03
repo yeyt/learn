a4faeaa1e94d152522e4931778cdaf5ccb229cf8
yeyt <939252296@qq.com>
2018-01-03 23:24:16 +0800

    freertos

---
+++ C-code/stm32f4/FREERTOS/1.移植/HARDWARE/LED/task_define.h
@@ -0,0 +1,42 @@
+#ifndef __TASK_DEFINE_H
+#define __TASK_DEFINE_H
+#include "FreeRTOS.h"
+#include "task.h"
+
+//任务优先级
+#define START_TASK_PRIO		1
+//任务堆栈大小	
+#define START_STK_SIZE 		128  
+//任务句柄
+TaskHandle_t StartTask_Handler;
+//任务函数
+void start_task(void *pvParameters);
+
+//任务优先级
+#define LED0_TASK_PRIO		2
+//任务堆栈大小	
+#define LED0_STK_SIZE 		50  
+//任务句柄
+TaskHandle_t LED0Task_Handler;
+//任务函数
+void led0_task(void *pvParameters);
+
+//任务优先级
+#define LED1_TASK_PRIO		3
+//任务堆栈大小	
+#define LED1_STK_SIZE 		50  
+//任务句柄
+TaskHandle_t LED1Task_Handler;
+//任务函数
+void led1_task(void *pvParameters);
+
+//任务优先级
+#define FLOAT_TASK_PRIO		4
+//任务堆栈大小	
+#define FLOAT_STK_SIZE 		128
+//任务句柄
+TaskHandle_t FLOATTask_Handler;
+//任务函数
+void float_task(void *pvParameters);
+
+#endif
---
+++ C-code/stm32f4/FREERTOS/1.移植/USER/main.c
@@ -0,0 +1,72 @@
+#include "sys.h"
+#include "delay.h"
+#include "usart.h"
+#include "led.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "task_define.h"
+
+
+int main(void)
+{ 
+ 
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);//设置系统中断优先级分组4
+	delay_init(168);		//初始化延时函数
+	uart_init(115200);     	//初始化串口
+	LED_Init();		        //初始化LED端
+		//创建开始任务
+  xTaskCreate((TaskFunction_t )start_task,            //任务函数
+              (const char*    )"start_task",          //任务名称
+              (uint16_t       )START_STK_SIZE,        //任务堆栈大小
+              (void*          )NULL,                  //传递给任务函数的参数
+              (UBaseType_t    )START_TASK_PRIO,       //任务优先级
+              (TaskHandle_t*  )&StartTask_Handler);   //任务句柄              
+  vTaskStartScheduler();          //开启任务调度
+}
+
+//开始任务任务函数
+void start_task(void *pvParameters)
+{
+    taskENTER_CRITICAL();           //进入临界区
+    //创建LED0任务
+    xTaskCreate((TaskFunction_t )led0_task,     	
+                (const char*    )"led0_task",   	
+                (uint16_t       )LED0_STK_SIZE, 
+                (void*          )NULL,				
+                (UBaseType_t    )LED0_TASK_PRIO,	
+                (TaskHandle_t*  )&LED0Task_Handler);          
+    //浮点测试任务
+    xTaskCreate((TaskFunction_t )float_task,     
+                (const char*    )"float_task",   
+                (uint16_t       )FLOAT_STK_SIZE, 
+                (void*          )NULL,
+                (UBaseType_t    )FLOAT_TASK_PRIO,
+                (TaskHandle_t*  )&FLOATTask_Handler);  
+    vTaskDelete(StartTask_Handler); //删除开始任务
+    taskEXIT_CRITICAL();            //退出临界区
+}
+
+//浮点测试任务
+void float_task(void *pvParameters)
+{
+	static float float_num=0.00;
+	while(1)
+	{
+		float_num+=0.01f;
+		printf("float_num的值为: %.4f\r\n",float_num);
+        vTaskDelay(1000);
+	}
+}
+
+//LED0任务函数 
+void led0_task(void *pvParameters)
+{
+    while(1)
+    {
+        LED0=~LED0;
+        vTaskDelay(500);
+    }
+}  
+
+
+
